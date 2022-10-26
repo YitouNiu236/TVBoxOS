1aa3f82148562665b7098ac5b9fb5a90f547012a
YitouNiu236 <410735666@qq.com>
2022-10-26 09:32:04 +0800

    Add default config

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -57,7 +57,8 @@ public class App extends MultiDexApplication {
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
-            Hawk.put(HawkConfig.PLAY_TYPE, 1);
+            Hawk.put(HawkConfig.PLAY_TYPE, 1);	// Player   0=系统, 1=IJK, 2=Exo
+			Hawk.put(HawkConfig.IJK_CODEC, "硬解码");// IJK Render 软解码, 硬解码
         }
     }
 
